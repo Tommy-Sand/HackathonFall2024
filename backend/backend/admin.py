237a69f1aa9d46f0014deab62fe6ec4a272f0d02
Moeid Khan <moeidmk@gmail.com>
2024-09-14 00:55:36 -0600

    changed cascade on deletion in application model, added admin stuff

--- backend/backend/admin.py
+++ backend/backend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Job, JobApplication
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name') 
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company_name', 'position') 
+   
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'job_posting', 'send_date', 'status')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
--- backend/backend/models.py
+++ backend/backend/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=200)
-    resume = models.CharField(max_length=32000000) #TODO Need a better way to represent resumes
+    #resume = models.CharField(max_length=32000000) #TODO Need a better way to represent resumes
+    resume = models.TextField()
 
 class Job(models.Model):
     company_name = models.CharField(max_length=200)
@@ -12,7 +13,8 @@ class Job(models.Model):
 # Create your models here.
 class JobApplication(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    job_posting = models.ForeignKey(Job, on_delete=models.CASCADE) #TODO Should not be cascade 
+    job_posting = models.ForeignKey(Job, on_delete=models.PROTECT)
+    #job_posting = models.ForeignKey(Job, on_delete=models.CASCADE) #TODO Should not be cascade 
     send_date = models.DateTimeField("date sent")
     status = models.CharField(max_length=200)
     interview_date = models.DateTimeField("date interview")
