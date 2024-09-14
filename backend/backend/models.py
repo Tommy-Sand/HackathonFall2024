5a24a17ce4ff88cb977f3443e6daf9e22eb3065e
Tommy Sandanasamy <rosan@ualberta.ca>
2024-09-14 08:19:20 -0600

    Removed commented out code and replaced some large CharFields with TextFields

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -2,22 +2,18 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=200)
-    #resume = models.CharField(max_length=32000000) #TODO Need a better way to represent resumes
     resume = models.TextField()
 
 class Job(models.Model):
     company_name = models.CharField(max_length=200)
     position = models.CharField(max_length=200)
-    description = models.CharField(max_length=1000000)
+    description = models.TextField()
 
-# Create your models here.
 class JobApplication(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job_posting = models.ForeignKey(Job, on_delete=models.PROTECT)
-    #job_posting = models.ForeignKey(Job, on_delete=models.CASCADE) #TODO Should not be cascade 
     send_date = models.DateTimeField("date sent")
     status = models.CharField(max_length=200)
     interview_date = models.DateTimeField("date interview")
     accept_date = models.DateTimeField("date accept")
-    resume = models.CharField(max_length=32000000)
-
+    resume = models.TextField()
