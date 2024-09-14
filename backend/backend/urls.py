b12e3158e529908e743cfd23a4b30e04805a4dbd
Tommy Sandanasamy <rosan@ualberta.ca>
2024-09-14 09:30:17 -0600

    Added urls and placeholder views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,5 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /backend/
     path("", views.index, name="index"),
+    # Get a user
+    # ex: /backend/tommy/
+    path("<name>/", views.user, name = "user"),
+    # List all user applications
+    # ex: /backend/tommy/applications/
+    path("<name>/applications", views.user_applications, name = "applications"),
+    # Get a specific application
+    # ex: /backend/tommy/applications/5 the 5th application a user ever made
+    path("<name>/applications/<int:application_id>", views.user_application, name = "applications"),
+    # Lists all jobs available
+    # ex: /backend/jobs/
+    path("jobs/", views.jobs, name = "jobs"),
+    # Get a specific job posting
+    # ex: /backend/jobs/5
+    path("jobs/<int:job_id>/", views.job, name = "job"),
+    path("jobs/<int:job_id>/apply/<name>", views.apply_to_job, name = "apply")
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Is this setup properly?")
+
+def user(request, name):
+    return HttpResponse("/backend/<name> unimplemented")
+
+def user_applications(request, name):
+    return HttpResponse("/backend/<name>/applications unimplemented")
+
+def user_application(request, name, application_id):
+    return HttpResponse("/backend/<name>/applications/<int:application_id> unimplemented")
+
+def jobs(request):
+    return HttpResponse("/backend/jobs/ unimplemented")
+
+def job(request, job_id):
+    return HttpResponse("/backend/jobs/<int:job_id>/ unimplemented")
+
+def apply_to_job(request, name, job_id):
+    return HttpResponse("jobs/<int:job_id>/apply/<name> unimplemented")
